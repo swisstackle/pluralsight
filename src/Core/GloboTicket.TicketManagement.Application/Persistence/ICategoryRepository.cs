edeb563b7821dc11e0ebe762f079b5ed68c9a612
Alain Georges Schaerer <alainsch@buffalo.edu>
2023-04-04 15:01:01 -0400

    Added Repository Interfaces for Repository Architecture

---
+++ src/Core/GloboTicket.TicketManagement.Application/Persistence/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+using GloboTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Contracts.Persistence {
+	public interface ICategoryRepository : IAsyncRepository<Category>  {
+	}
+}
